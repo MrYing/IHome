43e615b600a25182bcb4f9f934001a4e09a9549c
Mr.D <duanlikang_i@didichuxing.com>
2017-08-21 17:10:20 +0800

    Vue Project Init

---
+++ src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+	<h2>{{Message}}</h2>
+</template>
+<script>
+	export default{
+		name: 'Login',
+		data(){
+			return {
+				Message:'1231'
+			}
+		}
+	}
+</script>
+<style scoped>
+	h2{
+		color:#ff0000;
+	}
+</style>
